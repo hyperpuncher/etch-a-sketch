eb8fde87fa948b019013915938ee7d7b94413336
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2022-11-28 16:40:49 +0300

    Change painting from hover to mousedown

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.container'); 
+const canvas = document.querySelector('.canvas'); 
 const btn = document.querySelector('button');
-let gridWidth = 10;
+let gridWidth = 20;
+let mouseDown = false;
 
 createCanvas();
-
 
 btn.addEventListener('click', () => {
   gridWidth = parseInt(prompt('Input grid size'));
   createCanvas();
 })
 
+canvas.addEventListener('mousedown', () => mouseDown = true);
+container.addEventListener('mouseup', () => mouseDown = false);
+
 
 function createCanvas() {
-  container.innerHTML = "";
+  canvas.innerHTML = "";
   let gridSize = Math.pow(gridWidth, 2);
-  let pixelSize = 960 / gridWidth;
+  let pixelSize = 960 / gridWidth - 2;
   
   for (let i = 0; i < gridSize; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
     square.addEventListener('mouseover', () => {
-      square.classList.add('painted');
+      if (mouseDown) {
+        square.classList.add('painted');
+      }
     })
     square.style.height = `${pixelSize}px`;
-    container.appendChild(square);
+    canvas.appendChild(square);
   }
-  container.style.gridTemplateColumns = `repeat(${gridWidth}, 0fr)`
+  canvas.style.gridTemplateColumns = `repeat(${gridWidth}, 0fr)`
+  canvas.addEventListener('dragstart', (e) => e.preventDefault());
 }
