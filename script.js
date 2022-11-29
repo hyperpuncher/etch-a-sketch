2e36f41a2882feadcbd05a2685612aae3f608204
hyper_puncher <39203616+hyperpuncher@users.noreply.github.com>
2022-11-29 13:03:53 +0300

    New features and refactoring

--- script.js
+++ script.js
@@ -1,21 +1,55 @@
 const container = document.querySelector('.container'); 
 const canvas = document.querySelector('.canvas'); 
-const btn = document.querySelector('button');
-let gridWidth = 20;
+const btnReset = document.querySelector('#btn-reset');
+const gridSlider = document.querySelector('#slider');
+const gridValue = document.querySelector('#sliderValue')
+const radioColor = document.querySelectorAll('input[name="color"]');
+const BLACK = 'rgb(0, 0, 0)';
+const WHITE = 'rgb(255, 255, 255)';
+let paintStyle;
 let mouseDown = false;
 
-createCanvas();
+radioColor.forEach(element => {
+  if (element.checked) paintStyle = element.value;
+  element.addEventListener('change', (e) => paintStyle = e.target.value);
+});
 
-btn.addEventListener('click', () => {
-  gridWidth = parseInt(prompt('Input grid size'));
-  createCanvas();
-})
+gridValue.textContent = `${gridSlider.value} x ${gridSlider.value}`;
+gridSlider.oninput = () => {
+  gridValue.textContent = `${gridSlider.value} x ${gridSlider.value}`;
+  createCanvas(gridSlider.value);
+}
+
+btnReset.addEventListener('click', () => createCanvas(gridSlider.value));
 
 canvas.addEventListener('mousedown', () => mouseDown = true);
 container.addEventListener('mouseup', () => mouseDown = false);
 
 
-function createCanvas() {
+function getRandomColor() {
+  return(`rgb(${Math.floor(Math.random() * 255)},
+   ${Math.floor(Math.random() * 255)}, 
+   ${Math.floor(Math.random() * 255)})`)
+}
+
+function darkerColor(color) {
+  let rgb = color.slice(4, -1).split(", ");
+  let darkerRGB = [];
+  rgb.forEach(color => darkerRGB.push(Math.floor(color - 25.5)));
+  return(`rgb(${darkerRGB[0]}, ${darkerRGB[1]}, ${darkerRGB[2]})`)
+}
+
+function addPaintStyle(square, e) {
+  if (paintStyle === 'rainbow') {
+    square.style.backgroundColor = getRandomColor();
+  } else if (paintStyle === 'greyscale') {
+    square.style.backgroundColor = darkerColor(e.target.style.backgroundColor);
+  } else {
+    square.style.backgroundColor = BLACK;
+  }
+}
+
+function createCanvas(gridWidth) {
   canvas.innerHTML = "";
   let gridSize = Math.pow(gridWidth, 2);
   let pixelSize = 960 / gridWidth - 2;
@@ -23,14 +57,20 @@ function createCanvas() {
   for (let i = 0; i < gridSize; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
-    square.addEventListener('mouseover', () => {
-      if (mouseDown) {
-        square.classList.add('painted');
-      }
+    square.addEventListener('mousedown', (e) => {
+      addPaintStyle(square, e);
     })
+    square.addEventListener('mouseover', (e) => {
+      if (mouseDown) {
+        addPaintStyle(square, e);
+      }
+    });
     square.style.height = `${pixelSize}px`;
+    square.style.backgroundColor = WHITE;
     canvas.appendChild(square);
   }
   canvas.style.gridTemplateColumns = `repeat(${gridWidth}, 0fr)`
   canvas.addEventListener('dragstart', (e) => e.preventDefault());
 }
+
+createCanvas(gridSlider.value);
